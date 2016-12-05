64c30b87ac751316e52f43dc45fda6a850c78340
bsoptei <bsoptei@gmail.com>
2016-12-05 10:33:35 +0100

    fixed git

---
+++ src/com/greenfox/exams/java/BlackJack.java
@@ -0,0 +1,72 @@
+package com.greenfox.exams.java;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+/**
+ * Created by Balazs Soptei on 2016.12.05. Java exam
+ */
+public class BlackJack extends JPanel {
+    public JButton reset;
+    public JButton drawCard;
+    public Player house;
+    public Player user;
+    public JLabel housePlayed;
+    public JLabel userPlayed;
+    public Deck deck;
+
+    public BlackJack() {
+        createElements();
+        addButtonActionListener();
+        addElementsToPanel();
+        this.house = new Player();
+        this.user = new Player();
+        this.deck = new Deck();
+    }
+
+    public void createElements() {
+        reset = new JButton("New Game");
+        drawCard = new JButton("Draw a drawCard");
+        housePlayed = new JLabel("House played: ");
+        userPlayed = new JLabel("User played:");
+    }
+
+    public void addButtonActionListener(){
+        ListenForButton buttonListener = new ListenForButton();
+        reset.addActionListener(buttonListener);
+        drawCard.addActionListener(buttonListener);
+    }
+
+    public void addElementsToPanel() {
+        this.add(userPlayed);
+        this.add(housePlayed);
+        this.add(reset);
+        this.add(drawCard);
+    }
+    public class ListenForButton implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == reset) {
+                deck = new Deck();
+                user.played = new ArrayList<>();
+                house.played = new ArrayList<>();
+            } else {
+                if (!(deck.draw() == null)) {
+                    user.add(deck.draw());
+                    if (house.sum() < 17) {
+                        house.add(deck.draw());
+                    }
+                }
+            }
+            update();
+        }
+    }
+
+    private void update() {
+        housePlayed.setText("House played: " + house.showCards());
+       userPlayed.setText("User played: " + user.showCards());
+    }
+}
---
+++ src/com/greenfox/exams/java/Card.java
@@ -0,0 +1,19 @@
+package com.greenfox.exams.java;
+
+/**
+ * Created by Balazs Soptei on 2016.12.05. Java exam
+ */
+public class Card {
+    public String color;
+    public Integer value;
+
+    public Card(String color, Integer value) {
+        this.color = color;
+        this.value = value;
+        System.out.println("created card" + color + value);
+    }
+
+    public String getValue(){
+        return String.format("%s %d", color, value);
+    }
+}
